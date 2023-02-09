8e3243e3a0c52dfb767f2fe423792ecf068de3ac
Konstantin Dinev <kdinev@mail.bw.edu>
2023-02-09 11:16:45 +0200

    chore(*): adding chart placeholder

--- src/app/statistics/statistics.component.spec.ts
+++ src/app/statistics/statistics.component.spec.ts
@@ -1,6 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { StatisticsComponent } from './statistics.component';
 
@@ -10,8 +8,7 @@ describe('StatisticsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ StatisticsComponent ],
-      imports: [ NoopAnimationsModule, FormsModule ]
+      declarations: [ StatisticsComponent ]
     })
     .compileComponents();
   });
--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,20 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { FinancialService } from '../services/financial.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.scss']
 })
-export class StatisticsComponent implements OnInit {
-  public financialBoxOfficeRevenue: any = null;
-
-  constructor(
-    private financialService: FinancialService,
-  ) {}
-
-  ngOnInit() {
-    // depending on implementation, data subscriptions might need to be unsubbed later
-    this.financialService.getData('BoxOfficeRevenue').subscribe(data => this.financialBoxOfficeRevenue = data);
-  }
+export class StatisticsComponent {
 }
